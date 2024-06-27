d89635e666b66eb907a5c86fb3bd8776ad395b26
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-26 19:20:16 -0700

    check dict func

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from base_caching import BaseCaching
+"""first tutoiral of retierve chace from dict"""
+
+
+class BasicCache(BaseCaching):
+    """inherit from the parent class BaseCaching"""
+    def put(self, key, item):
+        """Put in the dict"""
+        if key and item:
+            if key not in self.cache_data.keys():
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """Get from the dict"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
