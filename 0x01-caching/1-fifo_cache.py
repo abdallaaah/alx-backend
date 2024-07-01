0dc2f3e361239bf634c94f302ad89b631ff6e803
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-30 18:38:18 -0700

    check task 1 fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
+    """inherit from the parent basechae"""
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
@@ -12,15 +13,15 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """the put in the dict function"""
         if key is None or item is None:
-            pass
-        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            return None
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             discard = self.cache_data.popitem(last=False)
             print(f"DISCARD: {discard[0]}")
         self.cache_data[key] = item
-        # print(f"{key} addedd")
 
     def get(self, key):
         """the get function from chache dict"""
 
-        if key is not None or key not in self.cache_data.keys():
-            return self.cache_data[key]
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-"""first tutoiral of retierve chace from dict"""
+"""Create a class FIFOCache that inherits from BaseCaching"""
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
-    """inherit from the parent class BaseCaching"""
     def __init__(self):
-
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Put in the dict"""
-        if key is not None and item is not None:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discard, _ = self.cache_data.popitem(last=True)
-                print(f"DISCARD: {discard}")
-            self.cache_data[key] = item
+        """the put in the dict function"""
+        if key is None or item is None:
+            return None
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {discard[0]}")
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Get from the dict"""
-        if not key or key not in self.cache_data.keys():
+        """the get function from chache dict"""
+
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
