03981e5b74e17d7a86b9999b836e34bed0c686f5
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-06-30 19:11:22 -0700

    check mru task

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Create a class FIFOCache that inherits from BaseCaching"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """inherit from the parent basechae"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """the put in the dict function"""
+        if key is None or item is None:
+            return None
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discard[0]}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """the get function from chache dict"""
+
+        if key is None or key not in self.cache_data.keys():
+            return None
+        x = self.cache_data[key]
+        self.cache_data.move_to_end(key, last=False)
+        return x
